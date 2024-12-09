f5ac276aa33af6bf8be1801a39641b740f66170f
Jecheon Ryu <rjc1704@gmail.com>
2024-12-09 11:30:57 +0900

    일부 보충

--- 1_primitive.ts
+++ 1_primitive.ts
@@ -6,5 +6,9 @@
   const notDefined: undefined = undefined; // undefined 타입
 
   const anyValue: any = "Hello"; // any 타입, 어떤 타입이든 될 수 있음
-  const unknownValue: unknown = 42; // unknown 타입, 어떤 타입이든 될 수 있지만 사용 전에 타입 검사가 필요함
+  const unknownValue: unknown = 42;
+  // unknown 타입, 어떤 타입이든 될 수 있지만 런타임에 타입 검사해야 사용할 수 있음.
+  if (typeof unknownValue === "number") {
+    console.log(unknownValue + 1);
+  }
 }
